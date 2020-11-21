2aba594437ca8d2a69214ee6641a437f898e607d
John J. Workman <johnjworkman@gmail.com>
2020-11-20 23:35:04 -0500

    Add rsync local backup script

---
+++ back.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env sh
+
+hash rsync 2>&- || { echo "requires rsync - aborting"; exit 1; }
+
+VERSION=`rsync --version | head -n 1 | perl -ne '$_ =~ m/version ([0-9])\./; print "$1\n";'`
+
+if [ ! "$VERSION" -ge 3 ]; then
+  echo "requires rsync version 3 or later - aborting"
+  exit 1
+fi
+
+SRC="$HOME/" # N.B. trailing slash so rsync doesn't create as subdir
+
+if [ ! -r "$SRC" ]; then
+  echo "cannot read from source $SRC - aborting"
+  exit 1
+fi
+
+DISK="/Volumes/Backup"
+
+if [ ! -d "$DISK" ]; then
+  echo "cannot access disk at $DISK - aborting"
+  exit 1
+fi
+
+NAME=`echo "$HOME" | rev | cut -d'/' -f1 | rev` # /foo/bar/baz/name => name
+DATE=`date '+%Y-%m-%d'`
+DEST="$DISK/$NAME-$DATE/" # .../name-2020-11-20/
+
+if [ -d "$DEST" ]; then
+  echo "destination $DEST already exists - aborting"
+  exit 1
+fi
+
+mkdir $DEST
+
+if [ ! -w "$DEST" ]; then
+  echo "cannot write to destination $DEST - aborting"
+  exit 1
+fi
+
+# rsync --dry-run \
+rsync \
+  --archive \
+  --human-readable \
+  --prune-empty-dirs \
+  --stats \
+  --progress \
+  --exclude ".DS_Store" \
+  --exclude ".localized" \
+  --exclude "/.*" \
+  --exclude "/Applications" \
+  --exclude "/Library" \
+  --exclude "/Public" \
+  ${SRC} ${DEST}
+
+exit 0
